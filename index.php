0e3692bbbff8f1fb08175a5ef9be64266c74b9f9
cosimo <ballerini.co@gmail.com>
2024-09-24 10:34:39 +0200

    finito

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-   <?php
-        $var = rand(0,100);
-
-        if($var%2==0)
-        {
-            echo "<p>il numero $var è pari</p>";
-        }
-        else
-        {
-            echo "<p>il numero $var è dispari</p>";
-        }
-
-        
-   ?>
+    <ul>
+        <?php
+            $N = 28;
+            echo "<h1>Divisori di $N</h1>";
+            
+            for ($i = 1; $i < $N; $i++) {
+                if ($N % $i == 0) {
+                    echo "<li>$i</li>";
+                }
+            }
+        ?>
+    </ul>
 </body>
 </html>
